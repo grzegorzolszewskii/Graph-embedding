e5561dba9baca14d70c31b0e023acd64bc2cdadd
grzegorzolszewskii <grzegorz.olszewski24@gmail.com>
2022-02-25 13:31:36 +0100

    test zanurzania

--- model.py
+++ model.py
@@ -12,10 +12,10 @@ class Model(Module):
         self.dim = dim
         self.model = Embedding(n, dim, sparse=sparse)
 
-    def forward(self, inputs):
-        e = self.model(inputs)  # macierz 10x52xdim, to bedzie e to preds z pliku embed
-        o = e.narrow(1, 1, e.size(1) - 1)   # macierz bez 1 kolumny (10x51)
-        s = e.narrow(1, 0, 1).expand_as(o)  # macierz 10x51 - wiersz to powt. sie 1 kolumna z e
+    def forward(self, inputs):  # z inputs zrobi preds - z macierzy 10x52 zrobi sie 10x52xdim
+        e = self.model(inputs)  # macierz 10x52xdim, to bedzie e to preds z pliku embed - tu wykonuje sie zanurzenie
+        o = e.narrow(1, 1, e.size(1) - 1)   # macierz e bez 1 kolumny (10x51xdim)
+        s = e.narrow(1, 0, 1).expand_as(o)  # macierz 10x51xdim - wiersz to powt. sie 1 kolumna z e
         dist = self.manifold.distance(s, o)
         return dist.squeeze(-1)
 
--- testy.py
+++ testy.py
@@ -1,24 +1,53 @@
 import torch as th
-e = th.zeros(size=(10, 52))
-for i in range(0, 10):
-    for j in range(0, 52):
-        if j == 0:
-            e[i, j] = 99 + i
-        else:
-            e[i, j] = i + j
+from math import pi
 
-print("Wektor e: ", e)
-o = e.narrow(1, 1, e.size(1) - 1)  # wierzcholki bez 1 kolumny
-s = e.narrow(1, 0, 1).expand_as(o)
 
-print("Wektor o: ", o)
-print("Wektor s: ", s)
-print(len(s[0]))
+def vectors():
+    e = th.zeros(size=(10, 52))
+    for i in range(0, 10):
+        for j in range(0, 52):
+            if j == 0:
+                e[i, j] = 99 + i
+            else:
+                e[i, j] = i + j
+
+    print("Wektor e: ", e)
+    o = e.narrow(1, 1, e.size(1) - 1)  # wierzcholki bez 1 kolumny
+    s = e.narrow(1, 0, 1).expand_as(o)
+
+    print("Wektor o: ", o)
+    print("Wektor s: ", s)
+    print(len(s[0]))
+    return e, o, s
+
+
+def moredim_vectors(dim=2):
+    e = th.zeros(size=(10, 52, dim))
+    for i in range(0, 10):
+        for j in range(0, 52):
+            for k in range(0, dim):
+                if j == 0 and k == 0:
+                    e[i, j, k] = 99 + i
+                if j == 1 and k == 0:
+                    e[i, j, k] = 299 + i
+                if j != 0 and j != 1 and k == 0:
+                    e[i, j, k] = i + j
+                if k != 0:
+                    e[i, j, k] = pi
+
+    print("Wektor e: ", e)
+    o = e.narrow(1, 1, e.size(1) - 1)  # wierzcholki bez 1 kolumny
+    s = e.narrow(1, 0, 1).expand_as(o)
+
+    print("Wektor o: ", o)
+    print("Wektor s: ", s)
+    print(len(s[0]))
+    return e, o, s
 
 
 def distance(u, v):
     return ((u - v).pow(2)).pow(1 / 2).sum(dim=-1)
 
 
-dist = distance(o, s)
+dist = distance(moredim_vectors()[1], moredim_vectors()[2])
 print(dist.squeeze(-1))
