9762922ea3f72b75863d76fd308e9463107fe6bf
grzegorzolszewskii <grzegorz.olszewski24@gmail.com>
2022-04-25 17:22:30 +0200

    25.04

---
+++ gr_success_rate.py
@@ -0,0 +1,22 @@
+from bfs import bfs
+from greedy_routing import greedy_routing
+from graph_import import load_graph2
+import pandas as pd
+
+
+def gr_success_rate(graph, coordinates):
+    success = 0
+    for i in range(len(graph)):
+        for j in range(len(graph)):
+            if bfs(graph, i, j) == greedy_routing(graph, coordinates, i, j):
+                success += 1
+            else:
+                print(bfs(graph, i, j), greedy_routing(graph, coordinates, i, j))
+
+    return success/(len(graph)**2)
+
+
+if __name__ == '__main__':
+    graph = load_graph2(46, 'tree_graph')
+    coordinates = pd.read_csv('best_embedding', header=None)
+    print(gr_success_rate(graph, coordinates))
