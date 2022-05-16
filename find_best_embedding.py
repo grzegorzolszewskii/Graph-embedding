092cdaa292aebf6ce17b5c47a470295678b98a88
grzegorzolszewskii <grzegorz.olszewski24@gmail.com>
2022-05-16 11:28:26 +0200

    16.05 - find_best_emb v1

--- find_best_embedding.py
+++ find_best_embedding.py
@@ -1,42 +1,45 @@
 import torch as th
 import random as rand
-from graph_import import load_graph2
+from graph_import import load_graph
 from manifolds import Manifold
 from model import Model
 from train_function import train
 
 
-def find_best_emb(graph, manifolds, dims, lrs, epochs, loops):
+def find_best_emb(graph, manifold, dims, lrs, epochs, loops):
     loss_with_params = dict()
     for loop in range(loops):
-        for m in manifolds:
-            for d in dims:
-                for l in lrs:
-                    for e in epochs:
-                        model = Model(m, nodes_num, d)
-                        optimizer = th.optim.SGD(model.parameters(), lr=l)
-                        loss_list, weights = train(graph, model, optimizer, epochs=e, max_loss=3.5)
-                        loss_with_params[(min(filter(lambda x: x > 0, loss_list)))] = (m, d, l, e, loop, weights)
+        for d in dims:
+            for l in lrs:
+                for e in epochs:
+                    model = Model(manifold, nodes_num, d)
+                    optimizer = th.optim.SGD(model.parameters(), lr=l)
+                    loss_list, weights = train(graph, model, optimizer, epochs=e, max_loss=3.5)
+                    loss_with_params[(min(filter(lambda x: x > 0, loss_list)))] = (manifold, d, l, e, loop, weights)
 
     return min(loss_with_params), loss_with_params[min(loss_with_params)]
 
 
 if __name__ == '__main__':
     nodes_num = 46
-    graph = load_graph2(nodes_num, data='tree_graph')
+    graph = load_graph(nodes_num, data='tree_graph')
 
-    manifolds = [Manifold('euclidean')]
-    dims = [2, 3, 4, 5]
+    manifold_euclidean = Manifold('euclidean')
+    manifold_lorentz = Manifold('lorentz')
+
+    dims_euclidean = [2, 3, 4, 5]
+    dims_lorentz = [3]
+
     lrs = [0.1, 0.3, 0.5, 0.7, 1]
     epochs = [300]
     loops = 1
 
-    loss, params = find_best_emb(graph, manifolds, dims, lrs, epochs, loops)
+    loss, params = find_best_emb(graph, manifold_euclidean, dims_euclidean, lrs, epochs, loops)
     dim = params[1]
     coordinates = params[5]
 
-    if loss < 3.4:
-        with open('best_embedding', 'w') as file:
+    if loss < 4:
+        with open('ok_embedding', 'w') as file:
             for i in range(len(graph)):
                 for j in range(dim):
                     file.write(str(coordinates[i][j].item()))
