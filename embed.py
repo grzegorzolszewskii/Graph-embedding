0f41e398e95b69dc96511dd23a0c04500fb7fdb5
grzegorzolszewskii <grzegorz.olszewski24@gmail.com>
2022-09-28 16:17:54 +0200

    poprawki argparse

--- embed.py
+++ embed.py
@@ -9,20 +9,7 @@ import argparse
 import pandas as pd
 
 
-parser = argparse.ArgumentParser(description="Pojedyncze zanurzenie wybranego grafu w przestrzen metryczna")
-parser.add_argument('-g', '--graph', type=str, required=True, help='zanurzany graf')
-parser.add_argument('-gs', '--graph_size', type=int, required=True, help='rozmiar grafu')
-parser.add_argument('-m', '--manifold', type=str, required=True, help='przestrzen metryczna')
-parser.add_argument('-dim', '--dim', type=int, required=True, help='liczba wymiarow przestrzeni')
-parser.add_argument('-lr', '--lr', type=float, required=True, help='wspolczynnik uczenia')
-parser.add_argument('-e', '--epochs', type=int, help='liczba epok')
-parser.add_argument('-a', '--alpha', type=float, help='parametr alpha')
-parser.add_argument('-loss', '--max_loss', type=float, help='maksymalna wartosc funkcji kosztu')
-args = parser.parse_args()
-
-
 # dla roznych parametrow otrzymuje loss fun i wspolrzedne - wykonuje jedno zanurzenie
-
 def embed(graph, manifold, dim, lr, epoch, alpha, max_loss):
     model = Model(manifold, len(graph), dim, alpha)
     if manifold.manifold_type == 'euclidean':
@@ -35,6 +22,17 @@ def embed(graph, manifold, dim, lr, epoch, alpha, max_loss):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Pojedyncze zanurzenie wybranego grafu w przestrzen metryczna")
+    parser.add_argument('-g', '--graph', type=str, required=True, help='zanurzany graf')
+    parser.add_argument('-gs', '--graph_size', type=int, required=True, help='rozmiar grafu')
+    parser.add_argument('-m', '--manifold', type=str, required=True, help='przestrzen metryczna')
+    parser.add_argument('-dim', '--dim', type=int, required=True, help='liczba wymiarow przestrzeni')
+    parser.add_argument('-lr', '--lr', type=float, required=True, help='wspolczynnik uczenia')
+    parser.add_argument('-e', '--epochs', type=int, help='liczba epok')
+    parser.add_argument('-a', '--alpha', type=float, help='parametr alpha')
+    parser.add_argument('-loss', '--max_loss', type=float, help='maksymalna wartosc funkcji kosztu')
+    args = parser.parse_args()
+
     graph = load_graph(args.graph_size, args.graph)
     manifold = Manifold(args.manifold)
     loss, coordinates = embed(graph, manifold, args.dim, args.lr, args.epochs, args.alpha, args.max_loss)
